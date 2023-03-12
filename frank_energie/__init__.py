446c5ffc2438c844a44eaa02af1604da2bfb50a7
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2023-03-12 11:31:47 +0100

    Delete frank_energie directory (#9)

--- frank_energie/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Frank Energie API library."""
-from .frank_energie import FrankEnergie
-from .models import Price, PriceData
-
-__all__ = [
-    "FrankEnergie",
-    "Price",
-    "PriceData",
-]
