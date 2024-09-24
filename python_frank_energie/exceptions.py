4c50a6cf54c602515f2530693bd9ee63ef706a59
HiDiHo <96914811+HiDiHo01@users.noreply.github.com>
2024-09-24 22:51:53 +0200

    Update exceptions.py

--- python_frank_energie/exceptions.py
+++ python_frank_energie/exceptions.py
@@ -1,17 +1,34 @@
 """Exceptions for the Frank Energie API."""
 
-
 class FrankEnergieException(Exception):
     """Base exception."""
-
 
 class AuthRequiredException(FrankEnergieException):
     """Authentication required for this request."""
 
-
 class AuthException(FrankEnergieException):
     """Authentication/login failed."""
 
-
 class RequestException(FrankEnergieException):
     """Request failed."""
+
+class NoSuitableSitesFoundError(Exception):
+    """Request failed."""
+
+class FrankEnergieError(Exception):
+    """Base class for all FrankEnergie-related errors."""
+
+class LoginError(FrankEnergieError):
+    """Raised when login to FrankEnergie fails."""
+
+class NetworkError(FrankEnergieError):
+    """Raised for network-related errors in FrankEnergie."""
+
+class RequestException(Exception):
+    """Custom exception for request errors."""
+
+class SmartTradingNotEnabledException(Exception):
+    """Exception raised when smart trading is not enabled for the user."""
+
+class ConnectionException(FrankEnergieError):
+    """Raised for network-related errors in FrankEnergie."""
